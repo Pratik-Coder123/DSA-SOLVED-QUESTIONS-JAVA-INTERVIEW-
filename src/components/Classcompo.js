9983b7dfb4fa49863e27189d2809caa00dac4bcb
Pratik-Coder123 <pratikghule8329@gmail.com>
2022-09-19 12:37:39 +0530

    first

---
+++ src/components/Classcompo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export class Classcompo1 extends React.Component {
+
+    render() {
+        return <p>This is a class Component</p>
+    }
+}
+   export class Classcompo2 extends React.Component {
+
+        render() {
+            return <p>This is a another class Component</p>
+        }
+    
+    }
+
